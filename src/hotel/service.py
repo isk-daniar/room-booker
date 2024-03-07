fd140810fb4b93919201e2b9be7ce7e5f3265152
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2024-03-07 10:38:00 +0500

    добавлен RoomsService для модели Rooms

--- src/hotel/service.py
+++ src/hotel/service.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import TypeAdapter
-from src.hotel.repository import HotelsRepository
-from src.hotel.schemas import HotelSchema
+from src.hotel.repository import HotelsRepository, RoomsRepository
+from src.hotel.schemas import HotelSchema, RoomSchema
 
 
 class HotelsService:
@@ -45,3 +45,47 @@ class HotelsService:
     @classmethod
     def delete_all(cls):
         HotelsRepository.delete_all()
+
+
+class RoomsService:
+    @classmethod
+    def add(cls, room: RoomSchema):
+        room_dict = room.to_dict_wo_id()
+        new_room = RoomsRepository.add(room_dict)
+        return TypeAdapter(RoomSchema).dump_python(new_room)
+
+    @classmethod
+    def get(cls, room_id: int):
+        room = RoomsRepository.get(room_id)
+        return TypeAdapter(RoomSchema).dump_python(room)
+
+    @classmethod
+    def list(
+            cls,
+            room_number: Optional[str] = None,
+            floor: Optional[str] = None,
+            cost: Optional[str] = None,
+            employed: Optional[str] = None,
+            number_beds: Optional[str] = None
+    ):
+        filter_by = {k: v for k, v in {"room_number": room_number, "floor": floor, "cost": cost, "employed": employed, "number_beds": number_beds}.items() if v}
+        rooms = RoomsRepository.list(filter_by)
+        return TypeAdapter(list[RoomSchema]).dump_python(rooms)
+
+    @classmethod
+    def count(cls) -> int:
+        count = RoomsRepository.count()
+        return count
+
+    @classmethod
+    def update(cls, room_id: int, room: RoomSchema):
+        room_dict = room.to_dict_wo_id()
+        RoomsRepository.update(room_id, room_dict)
+
+    @classmethod
+    def delete(cls, room_id: int):
+        RoomsRepository.delete(room_id)
+
+    @classmethod
+    def delete_all(cls):
+        RoomsRepository.delete_all()
